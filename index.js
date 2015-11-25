1387704d4c312e438781b440a225aa9b5847548c
madmaxom <maximilian.stader@studnets.fh-hagenberg.at>
2015-11-25 17:51:23 +0100

    implemented player list as table with player properties name and color

--- index.js
+++
@@ -1,9 +0,0 @@
-function AddPlayer() {
-    if (players.length >= 7) {
-        document.getElementById("add-player").disabled = true;
-    }
-    players.push(document.getElementById("player-name").innerText);
-    var playerlist = document.getElementById("player-list").innerHTML;
-    playerlist += "\n<li>"+document.getElementById("player-name").innerText+"</li>";
-    console.log(playerlist);
-}
--- js/index.js
+++ js/index.js
@@ -1,7 +1,99 @@
+var inputField = document.getElementById("player-name");
+inputField.addEventListener("keydown", checkInputField);
+var color = ["Red", "YellowGreen", "Blue", "Green", "Pink", "Orange", "Cyan", "BlueViolet"];
+var takenColor = [false, false, false, false, false, false, false, false];
+
+function hideTableIfEmpty() {
+    if (players.length == 0) {
+        var temp = document.getElementById("player-table");
+        temp.hidden = true;
+    }
+}
+
+function getColor() {
+    var i = 0;
+    while (takenColor[i]) {
+        i++;
+    }
+    takenColor[i] = true;
+    return color[i];
+}
+
+function checkInputField(event) {
+    if (event.keyCode == 13 && players.length <= 7) {
+        AddPlayer();
+    }
+}
+
+function contains(playerName) {
+    var contains = false;
+    players.forEach(function (entry) {
+        if (entry.name == playerName) {
+            contains = true;
+            return;
+        }
+    });
+    return contains;
+}
+
 function AddPlayer() {
-    if (players.length >= 7) 
-    document.getElementById("add-player").disabled = true;
-    if(players.length <= 8)
-    players.push(document.getElementById("player-name").valueOf);
-    console.log("test");
+    if (inputField.value != "" && !contains(inputField.value)) {
+        var player = { name: "", color: "" };
+        player.name = inputField.value;
+        players.push(player);
+        players[players.length - 1].color = getColor();
+        inputField.value = "";
+        if (players.length > 7) {
+            document.getElementById("add-player").disabled = true;
+        }
+        displayData();
+    } else {
+        if (inputField.value == "")
+            alert("Player-Name empty! Please put in at least one character.");
+        else
+            alert("Player already exists! Please choose another name.")
+    }
+    hideTableIfEmpty();
+}
+
+function displayData() {
+    var temp = document.getElementById("player-table");
+    temp.hidden = false;
+    var table = document.getElementById("player-table");
+    var i = 1;
+    while (table.rows.length > 1) {
+        table.deleteRow(i);
+    }
+    i = 1;
+    players.forEach(function (entry) {
+        var row = table.insertRow(i);
+        row.style.backgroundColor = entry.color;
+        var cell0 = row.insertCell(0);
+        var cell1 = row.insertCell(1);
+        cell0.innerHTML = entry.name;
+        cell1.innerHTML = '<button class="btn btn-default" onclick="deleteElem(' + i + ')">Delete</button>';
+        i++;
+    });
+    hideTableIfEmpty();
+}
+
+function deleteElem(index) {
+    var playersNew = [];
+    var i = 1;
+    players.forEach(function (entry) {
+        if (index != i)
+            playersNew.push(entry);
+        else {
+            for (var j = 0; j < color.length; j++) {
+                if (entry.color == color[j]) {
+                    takenColor[j] = false;
+                    break;
+                }
+            }
+        }
+        i++;
+    });
+    document.getElementById("add-player").disabled = false;
+    players = playersNew;
+    displayData();
 }
