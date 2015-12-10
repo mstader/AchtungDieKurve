4bf7edfe7a3eac03c3cf5814edc23aaa469cb312
AnnaG <anna-maria.gruber@gmx.at>
2015-12-10 10:06:32 +0100

    Code has now commented and key-code is in process

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
-var maxPlayerNumber = 5;
-var inputField = document.getElementById("player-name");
-inputField.addEventListener("keydown", checkInputField);
-var color = ["Red", "YellowGreen", "Blue", "Green", "Pink", "Orange", "Cyan", "BlueViolet"];
-var takenColor = [false, false, false, false, false, false, false, false];
+//In this Index.js are all settings that have to be made before the game can start.
 
+var maxPlayerNumber = 5;//not more than 5 players are allowed to attend the game. 
+var inputField = document.getElementById("player-name");//players enter their names here
+var getKeyLeft = document.getElementById("left-key");//player enters the left-key, for controlling the game
+var getKeyRight = document.getElementById("right-key");//player enters the right-key, for controlling the game
+inputField.addEventListener("keydown", checkInputField);//EventListener added, to know if the inputField is empty or has some value
+getKeyLeft.addEventListener("keydown", checkKeyCode);//EventListener added, to check if the entered keycode is correct
+getKeyRight.addEventListener("keydown", checkKeyCode);//EventListener added, to check if the entered keycode is correct
+var color = ["Red", "YellowGreen", "Blue", "Green", "Pink", "Orange", "Cyan", "BlueViolet"];//predefined colors, for the players
+var takenColor = [false, false, false, false, false, false, false, false];// when color is taken, value turns into true
+
+//When there is no player, it hides the playertable
 function hideTableIfEmpty() {
     if (players.length == 0) {
         var temp = document.getElementById("player-table");
@@ -11,6 +18,7 @@ function hideTableIfEmpty() {
     }
 }
 
+//This function assignes the color to the players inorder
 function getColor() {
     var i = 0;
     while (takenColor[i]) {
@@ -20,12 +28,20 @@ function getColor() {
     return color[i];
 }
 
+//This function make sure that there are not more than the max amount of players addable to the playerlist
 function checkInputField(event) {
     if (event.keyCode == 13 && players.length < maxPlayerNumber) {
         AddPlayer();
     }
 }
 
+//FUNCTION that checks the key-code, to make sure the entered key-code is ok for playing
+function checkKeyCode(event){
+
+}
+
+
+//This function checks if there is already a player with the same name in the playerlist
 function contains(playerName) {
     var contains = false;
     players.forEach(function (entry) {
@@ -37,6 +53,7 @@ function contains(playerName) {
     return contains;
 }
 
+//This function adds a player to the playerlist, assignes a color to the player --> TO-DO: add the keys for left and right!!!
 function AddPlayer() {
     if (inputField.value != "" && !contains(inputField.value)) {
         var player = { name: "", color: "", keyLeft: '', keyRight: '', finished: false };
@@ -57,6 +74,7 @@ function AddPlayer() {
     hideTableIfEmpty();
 }
 
+//This function displayes the current playerlist, also the color that has been assigned to the player and a delete-button
 function displayData() {
     var temp = document.getElementById("player-table");
     temp.hidden = false;
@@ -78,6 +96,7 @@ function displayData() {
     hideTableIfEmpty();
 }
 
+//This function deletes the selected gamer out of the playerlist. --> through push on the "delete"-button
 function deleteElem(index) {
     var playersNew = [];
     var i = 1;
@@ -99,10 +118,10 @@ function deleteElem(index) {
     displayData();
 }
 
+//This function pushes Gamerentry to the right to make space for the canvas --> disable this function, if there is no gamer 
 function changePage() {
     var startPage = document.getElementById("start-page");
     var gamePage = document.getElementById("game-page");
-    //changePage
     if (startPage.style.display == "none") {
         startPage.style.display = "block";
         gamePage.style.display = "none";
